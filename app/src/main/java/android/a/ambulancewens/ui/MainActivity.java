5fa0ecc2a0fee233ae49d8db444a2115040673be
Ferre Vaes <ferre_vaes@hotmail.com>
2020-02-20 11:27:53 +0100

    Added MainActivity

---
+++ app/src/main/java/android/a/ambulancewens/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package android.a.ambulancewens.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.a.ambulancewens.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
