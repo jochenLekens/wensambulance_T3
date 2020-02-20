691e64bddaf75243894e992b27c5866645f319c5
Daan Pinxten <daan.pinxten@student.pxl.be>
2020-02-20 14:00:00 +0100

    recycler view made and dummy data added v2

--- app/src/main/java/android/a/ambulancewens/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/android/a/ambulancewens/adapters/RecyclerViewAdapter.java
@@ -1,10 +1,9 @@
-package android.a.ambulancewens.data.adapters;
+package android.a.ambulancewens.adapters;
 
-import android.Manifest;
+import android.a.ambulancewens.R;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -12,9 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.wao.com.Database.WaoDatabase;
-import android.wao.com.R;
-import android.wao.com.activities.MainActivity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -24,39 +20,27 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> implements
-        ActivityCompat.OnRequestPermissionsResultCallback {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private static final String TAG = "RecyclerViewAdapter";
     private static final int REQUEST_CALL = 1;
-    Button imageCall;
 
-
-    Dialog myDialog;
-    private int positionClicked;
-
-    private ArrayList<String> mImageNames = new ArrayList<>();
-    private ArrayList<String> mImages = new ArrayList<>();
-    private ArrayList<String> websites = new ArrayList<>();
-    private ArrayList<String> openHours;
+    private ArrayList<String> mDates  = new ArrayList<>();
+    private ArrayList<String> mTimes = new ArrayList<>();
+    private ArrayList<String> mLocations = new ArrayList<>();
     private Context mContext;
 
 
-    public void fillDummyData(){
-        mImageNames.add()
-    }
-    public RecyclerViewAdapter(ArrayList<String> mImageNames, ArrayList<String> mImages, ArrayList<String> websites, ArrayList<String> openHours, Context mContext) {
-        this.mImageNames = mImageNames;
-        this.mImages = mImages;
-        this.websites = websites;
-        this.mContext = mContext;
-        this.openHours = openHours;
 
+    public RecyclerViewAdapter(ArrayList<String> mDates, ArrayList<String> mTimes, ArrayList<String> mLocations, Context mContext) {
+        this.mDates = mDates;
+        this.mTimes = mTimes;
+        this.mLocations = mLocations;
+        this.mContext = mContext;
     }
 
     @NonNull
@@ -64,43 +48,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
-
         return viewHolder;
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
-
-        holder.imageName.setText(mImageNames.get(position));
-        Log.d(TAG, "onBindViewHolder: " + mImages.get(position));
-        int tempInt = getResId(mImages.get(position), R.drawable.class);
-        holder.imageView.setImageResource(tempInt);
-        holder.openHours.setText(openHours.get(position));
-
-
-        myDialog = new Dialog(mContext);
-        myDialog.setContentView(R.layout.stores_popup_layout);
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                positionClicked = position;
-                myDialog = fillDialogData(myDialog);
-                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
-                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show(); // laat even zien waar je op klikte
-                int visits = db.shopDAO().getAmountOfTimesVisited(mImageNames.get(position));
-                Log.d(TAG, "Voordat er een winkel gekozen is deze zoveel keer opgezocht: " + visits);
-
-                // Toast.makeText(mContext, visits, Toast.LENGTH_SHORT).show(); // laat even zien waar je op klikte
-                db.shopDAO().update(visits + 1, mImageNames.get(position));
-                Log.d(TAG, "Nadat er een winkel gekozen is: " + db.shopDAO().getShopByName(mImageNames.get(position)).visitCounter);
-
-                myDialog.show();
-            }
-        });
+        holder.date.setText(mDates.get(position));
+        holder.times.setText(mTimes.get(position));
+        holder.locations.setText(mLocations.get(position));
+        Log.d(TAG, "onBindViewHolder: " + mDates.get(position));
     }
 
     public static int getResId(String resName, Class<?> c) {
@@ -114,70 +71,26 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         }
     }
 
-    private Dialog fillDialogData(Dialog myDialog) {
-        imageCall = myDialog.findViewById(R.id.image_call);
-        TextView storeName = myDialog.findViewById(R.id.popupStorename);
-        TextView Monday = myDialog.findViewById(R.id.MondayTextView);
-        TextView TuesDay = myDialog.findViewById(R.id.TheusDayTextView);
-        TextView WednesDay = myDialog.findViewById(R.id.WednesDayTextView);
-        TextView ThursDay = myDialog.findViewById(R.id.ThursDayTextView);
-        TextView Friday = myDialog.findViewById(R.id.FridayTextView);
-        TextView Saturday = myDialog.findViewById(R.id.SaturDayTextView);
-        TextView Sunday = myDialog.findViewById(R.id.SunDayTextView);
-        Button websiteButton = myDialog.findViewById(R.id.websiteButton);
-
-        storeName.setText(mImageNames.get(positionClicked));
-        String openHours = db.shopDAO().getAll().get(positionClicked).open;
-        Monday.setText(openHours);
-        TuesDay.setText(openHours);
-        WednesDay.setText(openHours);
-        ThursDay.setText(openHours);
-        Friday.setText(openHours);
-        Saturday.setText(openHours);
-        Sunday.setText(openHours);
-
-        imageCall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                makePhoneCall();
-            }
-        });
-
-        websiteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = websites.get(positionClicked);
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                mContext.startActivity(i);
-            }
-        });
-
-
-        return myDialog;
-    }
-
-
     @Override
     public int getItemCount() {
-        return mImageNames.size();
+        return mTimes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private LinearLayout store_popup;
-        ImageView imageView;
-        TextView imageName;
-        TextView openHours;
+        TextView date;
+        TextView times;
+        TextView locations;
         RelativeLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.logoImage);
-            imageName = itemView.findViewById(R.id.storeNameTextview);
+            date = itemView.findViewById(R.id.datumTextView);
+            times = itemView.findViewById(R.id.OpeningTodayTextview);
+            locations = itemView.findViewById(R.id.locationTextView);
             parentLayout = itemView.findViewById(R.id.parent_layout);
-            store_popup = (LinearLayout) itemView.findViewById(R.id.popup_layout);
-            openHours = itemView.findViewById(R.id.OpeningTodayTextview);
+
     }
 
     }
--- app/src/main/java/android/a/ambulancewens/ui/MainActivity.java
+++ app/src/main/java/android/a/ambulancewens/ui/MainActivity.java
@@ -1,15 +1,46 @@
 package android.a.ambulancewens.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.a.ambulancewens.R;
+import android.a.ambulancewens.adapters.RecyclerViewAdapter;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> mDates  = new ArrayList<>();
+    private ArrayList<String> mTimes = new ArrayList<>();
+    private ArrayList<String> mLocations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.fillDummyData();
+        this.initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mDates, mTimes, mLocations, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    public void fillDummyData(){
+        mDates.add("19 januari 2020");
+        mDates.add("20 januari 2020");
+        mDates.add("21 januari 2020");
+        mTimes.add("08:00 - 19:00");
+        mTimes.add("12:00 - 20:00");
+        mTimes.add("09:00 - 12:00");
+        mLocations.add("Hasselt - Kempische Steenweg 13");
+        mLocations.add("Hasselt - Kerkstraat 3");
+        mLocations.add("Leuven - Vrijheidslaan 15");
     }
 }
